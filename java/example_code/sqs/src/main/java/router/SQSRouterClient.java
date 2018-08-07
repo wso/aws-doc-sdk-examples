4c0d2b6a1cc1fd9aabf6fb313672abcb2d3d571e
W S <walterso@gmail.com>
2018-08-07 20:54:27 +0000

    implement SQS-based RouterClient and pass test

--- java/example_code/sqs/src/main/java/router/SQSRouterClient.java
+++ java/example_code/sqs/src/main/java/router/SQSRouterClient.java
@@ -1,7 +1,28 @@
 package router;
+import com.amazonaws.services.sqs.*;
+import com.amazonaws.services.sqs.model.*;
+import java.util.*;
 
-public class SQSRouterClient implements RouterClient {
+public class SQSRouterClient implements RouterClient, AutoCloseable {
+    final String queueUrl;
+    final AmazonSQS sqs;
+    
+    public SQSRouterClient(String queueName) {
+        this.sqs = AmazonSQSClientBuilder.defaultClient();
+        queueUrl = sqs.getQueueUrl(queueName).getQueueUrl();
+    }
+    
     public void routeData(String clientData) {
+        SendMessageRequest request = new SendMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMessageBody(clientData);
+                
+        request.setMessageGroupId("messageGroup1");
+        
+        sqs.sendMessage(request);
+    }
+    
+    public void close() {
         
     }
 }
--- java/example_code/sqs/src/test/java/router/SQSRouterClientIT.java
+++ java/example_code/sqs/src/test/java/router/SQSRouterClientIT.java
@@ -1,11 +1,36 @@
 package router;
 
 import org.junit.Test;
+import com.amazonaws.services.sqs.*;
+import com.amazonaws.services.sqs.model.*;
+import java.util.*;
+
+import static org.junit.Assert.*;
 
 public class SQSRouterClientIT {
+    String QUEUE_NAME = "demo.fifo";
     
     @Test
     public void whenRouterClientSendsMessage_ThenQueueSizeShouldIncrease() {
+        String input = "{ \"sql\":\"UPSERT INTO TEST values (?,?,?)\",\"params\":[\"p1\",\"p2\",\"p3\"]}";
+        
+        try (SQSRouterClient testObject = new SQSRouterClient(QUEUE_NAME) ) {
+            testObject.routeData(input);
+        }
+        
+
+        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+
+        // receive messages from the queue
+        List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
+
+        assertEquals( 1, messages.size());
+        // delete messages from the queue
+        for (Message m : messages) {
+            System.out.println( m.getReceiptHandle());
+            assertEquals( input, m.getBody());
+        }
         
     }
 }
